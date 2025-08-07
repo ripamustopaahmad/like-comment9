016e9bccbeb6a75518ea58bbcf4d2f4944142641
ripamustopaahmad <ripamustopaahmad00@gmail.com>
2025-08-07 14:24:02 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,307 @@
+// data followers
+const followers = [
+  {
+    title: "ripamustopaahmad",
+    link: "https://www.instagram.com/ripamustopaahmad?igsh=MW40MHIzYjhqMGhveA==",
+  },
+  {
+    title: "sitinurrr012",
+    link: "https://www.instagram.com/sitinurrr012?igsh=c2ZzOWs4d2JsbzMy",
+  },
+  {
+    title: "aimaspupah16",
+    link: "https://www.instagram.com/aimaspupah16?igsh=MWx6Z3QxMXFnZGs1OQ==",
+  },
+  {
+    title: "qurotuluyun_fthlizar",
+    link: "https://www.instagram.com/qurotuluyun_fthlizar?igsh=MWltNGI3YjdtY2VqNw==",
+  },
+  {
+    title: "syilasusilawati_06",
+    link: "https://www.instagram.com/syilasusilawati_06?igsh=bHk0emFvMmh4b2ky",
+  },
+  {
+    title: "vikri_096x",
+    link: "https://www.instagram.com/vikri_096x?igsh=MWlibjRzZWVvdmI4eQ==",
+  },
+  {
+    title: "rab.dull1565",
+    link: "https://www.instagram.com/ab.dull1565?igsh=MXBmeWE1ZnA2ZjRndQ==",
+  },
+  {
+    title: "ropimuzaqi_03",
+    link: "https://www.instagram.com/ropimuzaqi_03?igsh=MW1sdjEwYnh3bzdrZg==",
+  },
+  {
+    title: "mhamd.fhmi16",
+    link: "https://www.instagram.com/mhamd.fhmi16?igsh=ajJ4OTA1cG9oamhx",
+  },
+  {
+    title: "cahyanarmd_011",
+    link: "https://www.instagram.com/cahyanarmd_011?igsh=MTV1c3JjaTBudGFrZA==",
+  },
+  {
+    title: "dikarahadii",
+    link: "https://www.instagram.com/dikarahadii?igsh=MXM2bTR2MWZsaXlxYg==",
+  },
+  {
+    title: "rifkitresna_027",
+    link: "https://www.instagram.com/rifkitresna_027?igsh=Z2UzeWU1ejk1Yjk0",
+  },
+  {
+    title: "aldiprmnsyyh",
+    link: "https://www.instagram.com/aldiprmnsyyh?igsh=YjltYXZwcHU2emN3",
+  },
+  {
+    title: "fz_yucan",
+    link: "https://www.instagram.com/fz_yucan?igsh=cXpiNjR2eGIzcmR5",
+  },
+  {
+    title: "whoisnuri1",
+    link: "https://www.instagram.com/whoisnuri1?igsh=amFwNWh3bGo4NGVt",
+  },
+  {
+    title: "ftraaa.06",
+    link: "https://www.instagram.com/ftraaa.06?igsh=aHJnMjBvaDZpZnVq",
+  },
+  {
+    title: "aki_aki_kasp",
+    link: "https://www.instagram.com/aki_aki_kasp?igsh=MTB5ZXpvZmc1ZG14MA==",
+  },
+  {
+    title: "_breathesin",
+    link: "https://www.instagram.com/_breathesin?igsh=MTh2b3kycnZseDkyNw==",
+  },
+  {
+    title: "ahii018",
+    link: "https://www.instagram.com/ahii018?igsh=ZGxna2FjazIzbmV6",
+  },
+  {
+    title: "rikinw01",
+    link: "https://www.instagram.com/rikinw01?igsh=MTVxam42OW9qc2xl",
+  },
+  {
+    title: "tegar0805",
+    link: "https://www.instagram.com/tegar0805?igsh=MTg2b3NidjN6ejRtYQ==",
+  },
+  {
+    title: "byalparsk_026",
+    link: "https://www.instagram.com/byalparsk_026?igsh=cTI5eWZ5dDg3dnR4",
+  },
+  {
+    title: "nawawii12_",
+    link: "https://www.instagram.com/nawawii12_?igsh=bjE0Mnc0MDlqNzNn",
+  },
+  {
+    title: "alwafsaquaticbf",
+    link: "https://www.instagram.com/alwafsaquaticbf?igsh=YW80ZXBxdGg5Nzg0",
+  },
+  {
+    title: "dzack_one06",
+    link: "https://www.instagram.com/dzack_one06?igsh=Z2JkNXRpOXp4MzVu",
+  },
+  {
+    title: "diaspurnama07",
+    link: "https://www.instagram.com/diaspurnama07?igsh=dzV6czg3cGtpZHp2",
+  },
+];
+// data sidebar
+const listData = [
+  {
+    fromTo: "1-50",
+    link: "https://ripamustopaahmad.github.io/like-comment1/",
+  },
+  {
+    fromTo: "51-100",
+    link: "https://ripamustopaahmad.github.io/like-comment2/",
+  },
+  {
+    fromTo: "101-150",
+    link: "https://ripamustopaahmad.github.io/like-comment3/",
+  },
+  {
+    fromTo: "151-200",
+    link: "https://ripamustopaahmad.github.io/like-comment4/",
+  },
+  {
+    fromTo: "201-250",
+    link: "https://ripamustopaahmad.github.io/like-comment5/",
+  },
+  {
+    fromTo: "251-300",
+    link: "https://ripamustopaahmad.github.io/like-comment6/",
+  },
+  {
+    fromTo: "301-350",
+    link: "https://ripamustopaahmad.github.io/like-comment7/",
+  },
+  {
+    fromTo: "351-400",
+    link: "https://ripamustopaahmad.github.io/like-comment8/",
+  },
+  {
+    fromTo: "401-450",
+    link: "https://ripamustopaahmad.github.io/like-comment9/",
+  },
+  {
+    fromTo: "451-500",
+    link: "https://ripamustopaahmad.github.io/like-comment10/",
+  },
+  {
+    fromTo: "501-526",
+    link: "https://ripamustopaahmad.github.io/like-comment11/",
+  },
+];
+
+const photos = [
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+  { src: `photos/foto`, alt: "Foto " },
+];
+
+if (window.innerWidth >= 500) {
+  alert(
+    "Harap menggunakan device mobile handphone untuk mengahsilkan tampilan yang baik!"
+  );
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.get("scroll") === "on") {
+  window.scrollTo(0, 300);
+}
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const sidebar = document.querySelector(".nav-toggle");
+const mainContent = document.querySelector(".main-content");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+  mainContent.classList.toggle("shift");
+});
+
+const closeBtn = document.querySelector(".close-sidebar");
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+  mainContent.classList.toggle("shift");
+});
+
+setInterval(function () {
+  document.querySelector(".button").classList.add("gibeg");
+  setTimeout(function () {
+    document.querySelector(".button").classList.remove("gibeg");
+  }, 500);
+}, 5000);
+
+// looping sidebar
+
+const list = document.querySelector(".list-page");
+
+listData.forEach((pg) => {
+  const NewData = `
+         <li>
+              <a href="${pg.link}?scroll=on"
+                    ><div>Screenshot <span>${pg.fromTo}</span></div>
+                    <span
+                      ><i
+                        class="fa-solid fa-arrow-up-right-from-square"
+                    ></i></span
+              ></a>
+          </li>
+        `;
+  list.innerHTML += NewData;
+});
+
+// looping followers
+
+document.querySelector("#ttl").innerHTML = followers.length;
+const tableFollowers = document.querySelector(".table-followers");
+
+followers.forEach((fll) => {
+  const NewData = `
+          <li class="card-followers">
+                <a href="${fll.link}" class="title-card">
+                  <div class="icon-card text-ig"><i class="fa-brands fa-instagram"></i
+                ></div>
+                  <div>
+                    <div class="title-ig">${fll.title}</div>
+                    <div class="klik-lihat">klik untuk melihat!</div>
+                  </div>
+                </a>
+                <a href="${fll.link}" class="icon-act"><i class="fa-solid fa-arrow-up-right-from-square"></i
+              ></a>
+          </li>
+        `;
+  tableFollowers.innerHTML += NewData;
+});
+
+// looping gallery
+
+const gallery = document.getElementById("gallery");
+photos.forEach((photo, i) => {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery-item");
+  galleryItem.innerHTML = `
+                <div class="id">${i + 401}</div>
+                <img src="${photo.src} (${i + 1}).jpg" alt="${photo.alt}${i} ">
+                <span class="text"><i class="fa-solid fa-arrow-up-right-from-square"></i></span>
+            `;
+  gallery.appendChild(galleryItem);
+  galleryItem.addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    const modalImage = document.getElementById("modal-image");
+    modalImage.src = `${photo.src} (${i}).jpg`;
+    modal.style.display = "block";
+  });
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+});
